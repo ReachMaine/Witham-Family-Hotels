5d0d90bf937e4a36353b5912b28a08b98fc69aee
Linda "zig" Speight <linda@reachmaine.com>
2017-03-09 12:46:23 -0500

    add default email address.  probably will need to change that.

--- withamhotels/functions.php
+++ withamhotels/functions.php
@@ -54,3 +54,16 @@
 		$outstring .= '</p>';
 		echo $outstring;
 	}
+
+	/** changing default wordpres email settings */
+
+	add_filter('wp_mail_from', 'new_mail_from');
+	add_filter('wp_mail_from_name', 'new_mail_from_name');
+
+	function new_mail_from($old) {
+	 return 'kelly@withamhotels.com';
+	}
+
+	function new_mail_from_name($old) {
+	 return 'Witham Family Properites';
+	}
