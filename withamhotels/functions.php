462f1b9d07109742c09a792496b10ce43a254a49
Linda Speight <linda@reachmaine.com>
2017-03-20 10:25:31 -0400

    change from Email address to info @

--- withamhotels/functions.php
+++ withamhotels/functions.php
@@ -62,9 +62,9 @@
 	add_filter('wp_mail_from_name', 'new_mail_from_name');
 
 	function new_mail_from($old) {
-	 return 'kelly@withamhotels.com';
+	 return 'info@withamhotels.com';
 	}
 
 	function new_mail_from_name($old) {
-	 return 'Witham Family Properites';
+	 return 'Witham Family Hotels';
 	}
